7b4c2df8a4be3703ff4bb3db72ff46146394d8d6
AnfielGank14 <AnfielGank14>
2020-05-02 14:41:11 +0700

    clean repo interface web

--- front-end-mobile/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server       ="localhost";
-$username     ="root";
-$password     ="";
-$database     ="database";
-
-mysql_connect($server, $username, $password) or die("Koneksi tidak ada");
-mysql_select_db($database) or die("Database tidak ditemukan");
-?>
